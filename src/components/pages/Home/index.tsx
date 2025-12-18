d7888071fff1792100c9c0304c0bb99710197e9e
YessinEleuchi <yessineleuchi@iit.ens.tn>
2025-12-18 16:11:51 +0100

    integrate components in page

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View } from "react-native";
+import { profile, search, filter } from "../../../assets";
+import { HomeHeader, SearchBar } from "../../molecules";
+import {AppText} from "../../atoms";
 
 const Home: React.FC = () => {
+    const [q, setQ] = useState("");
+
     return (
-        <View style={styles.container}>
-        <Text style={styles.title}> Home </Text>
+        <View style={{ flex: 1 }}>
+            <HomeHeader avatar={profile} location="Sfax, Tunisia" />
+            <AppText variant={"body"} style={{marginHorizontal:30,
+            marginTop:15 , fontWeight:"bold"}}>
+                Good Morning, Yessine
+            </AppText>
+
+            <View style={{ marginTop: 16 }}>
+                <SearchBar
+                    value={q}
+                    onChangeText={setQ}
+                    placeholder="Search Coffee..."
+                    leftIcon={search}
+                    rightIcon={filter}
+                    onPressFilter={() => console.log("filter")}
+                />
             </View>
+        </View>
     );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: '600',
-    },
-});
